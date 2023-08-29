d599a70e7f955c0b40e55391c21bccb24d46701f
Guus Hamann <info@gustafson.nl>
2023-08-29 10:32:34 +0200

    add links and search

--- components/navbar.js
+++ components/navbar.js
@@ -1,29 +1,29 @@
 import Link from 'next/link'
 export default function Navbar() {
     return (
-        <div class="navbar bg-base-100">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                     <li><Link href="/grid">Home</Link></li>
                     <li><Link href="/docs">Docs</Link></li>
                     <li><Link href="/work">Work</Link></li>
                     </ul>
                 </div>
-                <a class="btn btn-ghost normal-case text-xl">Dashboard</a>
+                <a className="btn btn-ghost normal-case text-xl">Dashboard</a>
             </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal px-1">
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
                     <li><Link href="/grid">Home</Link></li>
                     <li><Link href="/docs">Docs</Link></li>
                     <li><Link href="/work">Work</Link></li>
                 </ul>
             </div>
-            <div class="navbar-end text-2xl font-bold mr-2">
-            <a class="btn btn-ghost normal-case text-xl">Gustafson</a> 
+            <div className="navbar-end text-2xl font-bold mr-2">
+            <a className="btn btn-ghost normal-case text-xl">Gustafson</a> 
             </div>
         </div>
     )
--- pages/grid/grid.js
+++ pages/grid/grid.js
@@ -3,34 +3,67 @@ import { useState, useEffect } from "react";
 function GridButton({ url, name }) {
   const icoUrl = `https://icons.duckduckgo.com/ip3/${url}.ico`
   const websiteUrl = `https://${url}`
-  return (<button class="btn">
+  return (<button className="btn bg-slate-700">
     <img height="16" width="16" src={icoUrl} />
     <Link href={websiteUrl} target="_blank">{name}</Link>
   </button>);
 }
+function ListButton({ url, name }) {
+  const websiteUrl = `https://${url}`
+  return (<div className="flex gap-4 btn bg-slate-700">
+    <a className="link" href={websiteUrl} target="_blank">{name}</a> - <a className="link" href={websiteUrl} target="_blank">{websiteUrl}</a>
+  </div>);
+}
+
 export default function links() {
   const [gridData, setGridData] = useState([])
-
+  const [listData, setListData] = useState([])
+  const [allData, setAllData] = useState([])
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch('/api/storeJSONData')
-      const data = await response.json();
-      setGridData(data);
+      let Idata = await response.json();
+
+      setAllData(Idata);
+
+
+
     }
     fetchData()
   }, [])
+  useEffect(() => {
+    setGridData(allData.filter(d => d.grid));
+    setListData(allData.filter(d => !d.grid));
+  },[allData])
 
+
+  function handleChange(event) {
+    setListData(allData.filter(d => !d.grid && d.name.includes(event.target.value)));
+    
+  }
   return (
-    <div className="flex justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 min-[2000px]:grid-cols-8 gap-4">
-        {gridData.map(buttonData => (
-          <GridButton url={buttonData.url} name={buttonData.name} />
-        ))}
+    <div className="grid gap-4 justify-center">
+
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 min-[2000px]:grid-cols-8 gap-4">
+          {gridData.map(buttonData => (
+            <GridButton url={buttonData.url} name={buttonData.name} key={buttonData.url} />
+          ))}
+
+
+        </div>
+
+      <input type="text" className="input w-full border border-slate-500 " placeholder="Search"  onChange={handleChange}  />
+      <div className="flex grow ">
+        
+          {listData.map(buttonData => (
+            <ListButton url={buttonData.url} name={buttonData.name} key={buttonData.url} />
+          ))}
 
 
       </div>
 
+      
     </div>
   )
 }
