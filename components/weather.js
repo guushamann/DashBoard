28089ded44089ece9a8e963a34dfc85e425d1c18
Guus Hamann <info@gustafson.nl>
2023-08-29 15:29:06 +0200

    allot

---
+++ components/weather.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+export default function Weather() {
+  useEffect(() => {
+    !(function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://weatherwidget.io/js/widget.min.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    })(document, "script", "weatherwidget-io-js");
+  }, []);
+  return (
+    <div className="weather">
+      <a
+        className="weatherwidget-io"
+        href="https://forecast7.com/nl/51d594d67/etten-leur/"
+        data-label_1="ETTEN-LEUR" data-label_2="Weer"
+        data-days="3"
+        data-theme="gray" 
+        data-basecolor="#334155"
+      >
+        ETTEN-LEUR Weer
+      </a>
+    </div>
+  );
+}
--- pages/grid/grid.js
+++ pages/grid/grid.js
@@ -1,36 +1,44 @@
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState, useEffect,useRef } from "react";
+import Weather from "../../components/weather";
 function GridButton({ url, name }) {
-  const icoUrl = `https://icons.duckduckgo.com/ip3/${url}.ico`
   const websiteUrl = `https://${url}`
-  return (<button className="btn bg-slate-700">
-    <img height="16" width="16" src={icoUrl} />
-    <Link href={websiteUrl} target="_blank">{name}</Link>
-  </button>);
+  const icoUrl = `https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://${url}&size=24`
+
+  return (<div className="flex rounded-lg  px-4 py-2 bg-slate-700 gap-4">
+    <div className="grow-0 "><img height="24" width="24" src={icoUrl} /></div>
+    <div className="grow uppercase "><Link href={websiteUrl} target="_blank">{name}</Link></div>
+  </div>);
 }
 function ListButton({ url, name }) {
   const websiteUrl = `https://${url}`
-  return (<div className="flex gap-4 btn bg-slate-700">
-    <a className="link" href={websiteUrl} target="_blank">{name}</a> - <a className="link" href={websiteUrl} target="_blank">{websiteUrl}</a>
+  return (<div className=" rounded-lg  p-4 bg-slate-700 w-full grid grid-cols-2 cursor-pointer" onClick={e=>window.open(websiteUrl)}>
+    <div className="link uppercase">{name}</div> <div className="link">{websiteUrl}</div>
   </div>);
 }
 
 export default function links() {
+  const inputRef = useRef(null);
   const [gridData, setGridData] = useState([])
   const [listData, setListData] = useState([])
   const [allData, setAllData] = useState([])
+  const escFunction = (event) => {
+    if ( event.keyCode == 27 ){
+      event.target.value=''
+      setListData(allData.filter(d => !d.grid));
+    }
+  };
+
 
   useEffect(() => {
+
     async function fetchData() {
       const response = await fetch('/api/storeJSONData')
       let Idata = await response.json();
-
       setAllData(Idata);
-
-
-
     }
     fetchData()
+    inputRef.current.focus();
   }, [])
   useEffect(() => {
     setGridData(allData.filter(d => d.grid));
@@ -39,11 +47,12 @@ export default function links() {
 
 
   function handleChange(event) {
-    setListData(allData.filter(d => !d.grid && d.name.includes(event.target.value)));
+    setListData(allData.filter(d => !d.grid && d.name.toLowerCase().includes(event.target.value.toLowerCase())));
     
   }
   return (
     <div className="grid gap-4 justify-center">
+        <Weather />
 
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 min-[2000px]:grid-cols-8 gap-4">
           {gridData.map(buttonData => (
@@ -53,8 +62,8 @@ export default function links() {
 
         </div>
 
-      <input type="text" className="input w-full border border-slate-500 " placeholder="Search"  onChange={handleChange}  />
-      <div className="flex grow ">
+      <input type="text" className="input w-full border border-slate-500 " ref={inputRef} autoFocus onKeyUp={escFunction} placeholder="Search"  onChange={handleChange}  />
+      <div className="grid gap-4  justify-start grid-cols-1">
         
           {listData.map(buttonData => (
             <ListButton url={buttonData.url} name={buttonData.name} key={buttonData.url} />
